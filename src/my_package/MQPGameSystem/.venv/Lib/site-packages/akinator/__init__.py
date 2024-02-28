b0b516b19436e8f3b587834644c6c13963883bc0
jjcaissie <jjcaissie@MQPVM.myguest.virtualbox.org>
2024-02-28 12:45:59 -0500

    catkin ws with ros nodes

---
+++ src/my_package/MQPGameSystem/.venv/Lib/site-packages/akinator/__init__.py
@@ -0,0 +1,22 @@
+"""
+Akinator API Wrapper
+~~~~~~~~~~~~~~~~~~~~
+
+A basic API wrapper for Akinator.
+
+:copyright: (c) 2019 NinjaSnail1080 & (c) 2022-present Omkaar
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = 'akinator'
+__author__ = 'Omkaar'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 Omkaar'
+__version__ = '1.1.1'
+
+
+import os
+
+from .akinator import Akinator
+from .exceptions import *
---
+++ temp/subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import rospy
+from std_msgs.msg import String
+
+def subscriberCallBack(data):
+    rospy.loginfo(rospy.get_caller_id() + "Recieved: %s", data.data)
+
+def listener():
+    rospy.init_node('subscriberNode', anonymous=True)
+    rospy.Subscriber("publisher", String, subscriberCallBack)
+    rospy.spin()
+
+#Run if run directly
+if __name__ == '__main__':
+    listener()
