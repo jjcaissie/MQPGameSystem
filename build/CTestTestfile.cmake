6990a0dfe5fac36c2c0331263efbf5873e2db969
Jayson Caissie <caissiejayson@gmail.com>
2024-03-19 15:23:24 -0400

    Removed redundant files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jjcaissie/Desktop/catkin_ws/src
-# Build directory: /home/jjcaissie/Desktop/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("my_package")
